90e82d1e0efa6495184e03ef08f433a887333463
Oluwaseun Onabajo <oluwaseun.onabajo@Terra-121.local>
2023-02-15 00:17:12 +0100

    constant-update

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,1 +1,2 @@
-export const BASE_URL = "https://twitteeapp.herokuapp.com/api/twitee/v1";
+export const BASE_URL = "https://mini-twittee-app.onrender.com/api/twitee/v1";
+// export const BASE_URL = "http://localhost:4000/api/twitee/v1";
